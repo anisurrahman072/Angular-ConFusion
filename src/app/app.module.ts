f9ddead084095bb2b2b6813ac5b4210818eb87d7
Md Anisur Rahman <anisurrahmanbup@gmail.com>
2020-06-11 23:54:40 +0600

    Components Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,31 @@
 /** This is our ROOT_MODULE file **/
 import { BrowserModule } from '@angular/platform-browser'; // age thekei cilo
 import { NgModule } from '@angular/core';    // age thekei cilo
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {FlexLayoutModule} from '@angular/flex-layout';
-import 'hammerjs';
-
 import { AppRoutingModule } from './app-routing.module';  // age thekei cilo
 import { AppComponent } from './app.component';  // age thekei cilo
 import { from } from 'rxjs';  // age thekei cilo
 
+import 'hammerjs';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+
+import { MenuComponent } from './menu/menu.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+
     BrowserAnimationsModule,
     FlexLayoutModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
