31f2df4a72d9189020dcdbefa516bd99d7fbc846
Md Anisur Rahman <anisurrahmanbup@gmail.com>
2020-06-14 22:01:35 +0600

    week 1 assignment 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,11 +15,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
 
 import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishdetailComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/comments.ts
@@ -0,0 +1,6 @@
+export class  Comments{
+    rating: number;
+    comment: any;
+    author: any;
+    date: any;
+}
