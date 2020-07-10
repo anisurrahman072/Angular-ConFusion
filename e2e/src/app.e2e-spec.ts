a78be616ad50bd033b0bdf65f6691c503835ed43
Md Anisur Rahman <anisurrahmanbup@gmail.com>
2020-07-10 12:36:33 +0600

    End-to-End Test

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -8,9 +8,28 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('conFusion app is running!');
+  it('should display message saying Ristornate Con Fusion', () => {
+    page.navigateTo('/');
+    expect(page.getParagraphText('h1')).toEqual('Ristorante Con Fusion');
+  });
+
+  it('should navigate to about page by clicking on link', () => {
+    page.navigateTo('/');
+    const navLink = page.getAllElement('a').get(1);
+    navLink.click();
+    expect(page.getParagraphText('h3')).toBe('About Us');
+  });
+
+  it('should enter a new comment for the first dish', () => {
+    page.navigateTo('/dishdetail/0');
+    const newAuthor = page.getElement('input[type=text]');
+    newAuthor.sendKeys('Shehab gsd');
+    const newComment = page.getElement('textarea');
+    newComment.sendKeys('fgh running.....');
+    const newSubmitButton = page.getElement('button[type=submit]');
+    newSubmitButton.click();
+
+    // browser.pause();  /// ERROR: pause() is not a function of browser protractor!!!!!
   });
 
   afterEach(async () => {
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,19 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
-  navigateTo(): Promise<unknown> {
-    return browser.get(browser.baseUrl) as Promise<unknown>;
+  navigateTo(link: string): Promise<unknown> {
+    return browser.get(link) as Promise<unknown>;
   }
 
-  getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
+  getParagraphText(selector: string): Promise<string> {
+    return element(by.css(selector)).getText() as Promise<string>;
+  }
+
+  getElement(selector: string){
+    return element(by.css(selector));
+  }
+
+  getAllElement(selector: string){
+    return element.all(by.css(selector));
   }
 }
